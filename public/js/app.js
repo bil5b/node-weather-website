b118f9eff43f7835c0d0fd663d24493f389983a2
Bilal Saeed Bhatti <bilalsaeedb@gmail.com>
2023-11-29 04:15:52 +0500

    Fix fetch statement

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/weather?address=').then((response) => {
+fetch('/weather?address=').then((response) => {
 	response.json().then((data) => {
 		if (data.error) {
 			return console.log(data.error)
