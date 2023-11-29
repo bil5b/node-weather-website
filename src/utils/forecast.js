12821951b903781aca0c2e1838206e14f5fc2f4a
Bilal Saeed Bhatti <bilalsaeedb@gmail.com>
2023-11-29 05:15:02 +0500

    Add humidity info in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,8 @@ const forecast = (lat, lon, callback) => {
 		} else if (body.error) {
 			callback('Unable to find location!', undefined)
 		} else {
-			callback(undefined, body.current.weather_descriptions[0] +'. It is currently ' + body.current.temperature + ' degree Celsius out. '
-			+ 'There is a ' + body.current.precip + '% chance of rain.')
+			callback(undefined, body.current.weather_descriptions[0] +'. It is currently ' + body.current.temperature + 
+				' degree Celsius out. There is a ' + body.current.precip + '% chance of rain. The humidity is ' + body.current.humidity + '%.')
 		}
 	})
 }
